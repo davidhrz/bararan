688d8de504d88a18863be3013de986d27cef989e
David Horozian <david.horozian@epita.fr>
2021-08-24 18:49:37 +0200

    added auto input with 500ms delay

--- popup/translate.js
+++ popup/translate.js
@@ -1,10 +1,39 @@
-const translateButton = document.querySelector("button");
 const textarea = document.querySelector("textarea");
-const outputArea = document.querySelector(".output-area");
+const historyArea = document.querySelector(".history-area");
 
-translateButton.addEventListener('click', () => {
-    let result = document.createElement("p");
-    result.nodeValue = textarea.value;
-    outputArea.appendChild(result);
-
+let timeoutId = 0;
+textarea.addEventListener('input', () => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(processWord, 500);
 });
+
+function translateWord() {
+    let headers = new Headers();
+    headers.append('User-Agent', 'python-requests/2.26.0');
+    headers.append('Accept-Encoding', 'gzip, deflate');
+    headers.append('Accept', '*/*');
+    headers.append('Connection', 'keep-alive');
+    var init = { method: 'GET',
+    headers: headers,
+    mode: 'same-origin'};
+    let request = new Request("https://bararanonline.com/բարև", init);
+    fetch(request).then((response) => {
+        console.log(response);
+    });
+}
+
+function addToHistory(word) {
+    let li = document.createElement("li");
+    li.textContent = word;
+    historyArea.querySelector("ul").appendChild(li);
+}
+
+function processWord() {
+    let input = textarea.value.trim();
+    if (input === "") {
+        return;
+    }
+    translateWord();
+    addToHistory(input);
+}
+
